d664727414ada7a57f568732127fd2505ac50dff
tabrianna <tabriannar@gmail.com>
2022-08-21 21:36:10 -0400

    final edit calc

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // These variable hold the numbers we want to do operations on and the name of the operation we want to perform.
 // They are expected to change so we use the "let" keyword.
+console.log ("html")
 let firstNum = null
 let secondNum = null
 let operation = null
 
 // this function takes in the number you type in the input field and saves it to the "firstNum" variable
 const saveFirstNumber = (num) => {
-  firstNum = parseInt(num)  
+  firstNum = parseInt(num) 
+  console.log("num", num) 
 }
 
 // this function takes in the number you type in the 2nd input field and saves it to the "secondNum" variable
 const saveSecondNumber = (num) => {
   // "parseInt" is a built in function in JS that converts a string/word into a number
   secondNum = parseInt(num)
+  console.log("num2", num)
 }
 
 // this function takes in two argument/numbers and returns the sum of them
@@ -30,22 +33,27 @@ const subtract = (numA, numB) => {
 // These variables are already defined but that don't point to functions. It's up to you to build the functions to complete your calculator use:
 
 const multiply = (numA, numB) => {
+  const multiplication = numA * numB
+  return multiplication
   // * to get a product then return it
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
-  console.log(numA, numB)
-}
 
-const divide = null
+} 
+
+const divide = (numA, numB) => {
+  const division = numA / numB
+  return division
+}
 // / to get a quotient,
 
 const modulus = null
 // and % to get a remainder.
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
-const changeOperation = (chosenOperation) => {
+function changeOperation(chosenOperation) {
   operation = chosenOperation
   // Use your Chrome Inspector Tool > Console Tab to see the "operation" that's logged
-  console.log(operation)
+  console.log("operation", operation)
 }
 
 // In order to show the user their results we have to access the DOM and stick in the value
@@ -64,13 +72,19 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
 }
+
+const compute = () => {
+  equals()
+  console.log("this is compute")
+}
+
 
